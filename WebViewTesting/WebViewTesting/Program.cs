d9f64a5daa2e6d92bb306ebd6cd78ae89a2ed023
Pramod <pramod.keeliputi@philips.com>
2021-08-04 18:11:46 +0530

    added sample project

---
+++ WebViewTesting/WebViewTesting/Program.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Remote;
+using System;
+
+namespace WebViewTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EdgeOptions edgeOptions = new EdgeOptions();
+            edgeOptions.UseChromium = true;
+            edgeOptions.UseWebView = true;
+            edgeOptions.BinaryLocation = @"C:\SampleApp\WebView2WpfBrowser.exe";
+            IWebDriver webDriver = new RemoteWebDriver(new Uri("http://127.0.0.1:4444"),edgeOptions);
+          
+        }
+    }
+}
